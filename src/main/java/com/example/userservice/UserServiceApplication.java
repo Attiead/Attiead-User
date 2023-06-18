4abab5639f9c93dee3cbce6f4506d0577e0a1186
groovyarea <095201a@naver.com>
2023-06-18 18:59:47 +0900

    feat : jib 추가

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,23 +3,29 @@ package com.example.userservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableEurekaClient
 @RestController
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
-	@GetMapping("/info")
-	public String info(@Value("${}") String port,
-					   @Value("${message.content}") String content) {
+    @GetMapping("/")
+    public String index() {
+        return "string";
+    }
 
-		return "User 서비스의 기본 동작 Port: {" + port + "}, content는 = "+content;
-	}
+    @GetMapping("/info")
+    public String info(@Value("${message.content}") String content) {
+
+        return "User 서비스의 기본 content는 = " + content;
+    }
 }
