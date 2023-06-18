dcd63ce4e75942419be339bc98294452c1cdc743
TaejinChang <tjchang@pentasecurity.com>
2023-06-18 16:37:05 +0900

    init commit

---
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -0,0 +1,25 @@
+package com.example.userservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@GetMapping("/info")
+	public String info(@Value("${}") String port,
+					   @Value("${message.content}") String content) {
+
+		return "User 서비스의 기본 동작 Port: {" + port + "}, content는 = "+content;
+	}
+}
