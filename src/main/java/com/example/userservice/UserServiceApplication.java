0bd57cadcd03f161dd73de417acd22b591ff87c2
groovyarea <095201a@naver.com>
2023-06-19 23:03:15 +0900

    fix : user 매핑 /user 추가

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableEurekaClient
+@RequestMapping("/user")
 @RestController
 public class UserServiceApplication {
 
