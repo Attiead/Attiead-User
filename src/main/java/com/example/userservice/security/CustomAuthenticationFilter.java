9702f54f0c093cfc6171b670289e3491f641d30d
TaejinChang <tjchang@pentasecurity.com>
2023-09-24 21:42:00 +0900

    feat: eof처리, custom exception message 정리

--- src/main/java/com/example/userservice/security/CustomAuthenticationFilter.java
+++ src/main/java/com/example/userservice/security/CustomAuthenticationFilter.java
@@ -26,7 +26,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
             UserAccountDto user = new ObjectMapper().readValue(request.getInputStream(), UserAccountDto.class);
             authRequest = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
         } catch (IOException exception) {
-            throw new UsernameNotFoundException("확인할 계정 정보가 없음");
+            throw new UsernameNotFoundException("로그인에 필요한 정보를 잘못 입력하셨습니다");
         }
 
         setDetails(request, authRequest);
