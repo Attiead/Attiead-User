95d4115a6ae39d8afb1dfc07d40f9a60b25bf902
TaejinChang <tjchang@pentasecurity.com>
2023-12-10 23:04:11 +0900

    [#19] feat: 미사용 @Validated 제거.

--- src/main/java/com/example/userservice/adapter/in/web/UserUpdateController.java
+++ src/main/java/com/example/userservice/adapter/in/web/UserUpdateController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Validated
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserUpdateController {
